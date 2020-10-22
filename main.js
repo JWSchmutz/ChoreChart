0016e8137036bac2a768fb10aad25fd2c53fba0b
Jeremiah Schmutz <Jeremiah.Schmutz@gmail.com>
2020-10-22 11:11:56 -0400

    added removing check functionality

--- main.js
+++ main.js
@@ -1,29 +1,35 @@
+//firebase
 var database = firebase.database();
-
-$(".check-box").on("click", function() {
-  console.log(
-    $(this)
-      .find(".invisible")
-      .attr("id")
-  );
-  //   firebase.database().ref('users/' + userId).set({
-  //     username: name,
-  //     email: email,
-  //     profile_picture : imageUrl
-  //   });
-  database.ref($(this).attr("id")).push(
-    $(this)
-      .find(".invisible")
-      .attr("id")
-  );
-  $(this)
-    .find(".invisible")
-    .removeClass("invisible");
+//on clicking a box
+$(".check-box").on("click", function () {
+  //if is already checked
+  if ($(this).hasClass("checked")) {
+    //mark as unchecked
+    $(this).removeClass("checked");
+    //make check invis
+    $(this).find(".fa-check").addClass("invisible");
+    //in progress
+    console.log($(this).attr("data-key"));
+    database.ref().child($(this).attr("data-key")).remove();
+    //otherwise
+  } else {
+    // add the checked value to firebase
+    database
+      .ref()
+      .push($(this).find(".invisible").attr("id"))
+      .then((snapshot) => $(this).attr("data-key", snapshot.key));
+    // make check visible
+    $(this).find(".invisible").removeClass("invisible");
+    // mark as checked
+    $(this).addClass("checked");
+  }
 });
-database.ref().on("value", function(snapshot) {
+//on page load, get all of the checked chores
+database.ref().once("value", function (snapshot) {
   for (var property in snapshot.val()) {
     $("#" + snapshot.val()[property]).removeClass("invisible");
+    $("#" + snapshot.val()[property])
+      .parent()
+      .addClass("checked");
   }
-
-  console.log(snapshot.val());
 });
